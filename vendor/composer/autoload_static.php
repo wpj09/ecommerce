377e083326a90996a30ffa1e823573e7b59d62e3
wpj09 <wpjwpj09@gmail.com>
2020-04-21 22:56:40 -0300

    autoload hcodebr classes

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit7a884cc41e4b9d018293b753a891849f
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInit7a884cc41e4b9d018293b753a891849f
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit7a884cc41e4b9d018293b753a891849f::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit7a884cc41e4b9d018293b753a891849f::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit7a884cc41e4b9d018293b753a891849f::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit7a884cc41e4b9d018293b753a891849f::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit7a884cc41e4b9d018293b753a891849f::$classMap;
 
