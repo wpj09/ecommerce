4ef0e70abe4b259f2ca38a42b40e882aa4672d55
wpj09 <wpjwpj09@gmail.com>
2020-04-25 00:34:31 -0300

    tela de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,10 +9,14 @@ class Page {
 	private $tpl;
 	private $options = [];
 	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
 		"data"=>[]
 	];
 
 	public function __construct($opts = array(), $tpl_dir = "/views/") {
+
+		//$this->defaults["data"]["session"] = $_SESSION;
 
 		$this->options = array_merge($this->defaults, $opts);
 
@@ -28,7 +32,7 @@ class Page {
 
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if ($this->options["header"] === true) $this->tpl->draw("header");
 
 	}
 
@@ -50,10 +54,9 @@ class Page {
 
 	}
 
-	public function __destruct()
-	{
+	public function __destruct() {
 
-		$this->tpl->draw("footer");
+		if ($this->options["footer"] === true) $this->tpl->draw("footer");
 
 	}
 }
